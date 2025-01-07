654d6ca9da4f133c74308e31f910ebecdac020fe
aksharthaker <aksharthaker1@gmail.com>
2025-01-07 15:43:24 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,124 @@
+a=5
+b=5
+c=a+b
+print("a is",a)
+print("b is",b)
+print(c)
+d=b-1
+print(d)
+print(a*b)
+print(a/b)
+
+#convert hours into minutes
+print("covert 1 hour into minutes")
+a=1
+b=a*60
+print(b)
+
+#convert dollars into ruppess
+a=int(input("Emter the ruppess:"))
+b=48*a
+print("dollars are:",b)
+
+#convert ruppess into dollars
+a=int(input("Emter the dollars:"))
+b=a/48
+print("dollars are:",b)
+
+#convert dollars into pounds
+a=int(input("Emter the dollars:"))
+b=48*a
+c=b/70
+print(c)
+
+#convert grams into kg
+a=int(input("Emter the grams"))
+b=a/1000
+print("kg are:",b)
+
+#convert kg into gm
+a=int(input("Emter the gram"))
+b=a*1000
+print("grams are:",b)
+
+#mb gb kb
+a=int(input("enter the bytes"))
+b=1000*a
+c=b*1000
+d=c*1000
+print("kb is",b,"mb is",c,"gb is",d)
+
+#convert celcius to farenhite
+a=int(input("Emter celsius"))
+b=(9/5 *a)+32
+print("faremhite:",b)
+
+#convert farenhite to celcius
+a=int(input("Emter farenhite"))
+b=5/9*(a-32)
+print("celcius",b)
+
+#intrest
+a=int(input("ENter the PRN"))
+b=a/100
+print(b)
+
+#area of square
+a=int(input("enter the length of square"))
+print("area is ",a*a)
+print("perimieter is",4*a)
+
+#area of rec
+b=int(input("enter the length of rectangle"))
+a=int(input("enter the length of rectangle"))
+print("area is ",a*b)
+print("perimieter is",2*(a+b))
+
+
+#area of circle
+b=int(input("enter the radius"))
+print("area is",(22/7) *b*b)
+
+#area of triNGLE
+b=int(input("enter the length of triangle"))
+a=int(input("enter the length of triangle"))
+print("area is ",(a*b)/2)
+
+#net salary
+a=int(input("enter the gross salary"))
+b=a/10
+c=a*3/100
+print("Net salary is",a+b-c)
+
+#net sales
+a=int(input("enter the gross sales"))
+b=a/10
+print("net sales are",a-b)
+
+#avg
+a=int(input("enter the first subject"))
+b=int(input("enter the second subject"))
+c=int(input("enter the third subject"))
+d=(a+b+c)/3
+print("avg is",c)
+
+#swap
+a=int(input("enter the first"))
+b=int(input("enter the second"))
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+
+
+
+
+
+
+      
+
+
+
+
+
+
